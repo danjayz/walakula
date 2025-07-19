adfb5a38778ade070f5423fdf5460ce6e15d08a2
Dananjaya <hello.danjay@gmail.com>
2025-07-19 23:54:02 +0530

    Initialize React + TypeScript + Vite frontend
    Add initial project structure for the 'walakula' frontend using React, TypeScript, and Vite. Includes configuration files, ESLint setup, basic assets, and dependencies for development with Material UI and related tooling.

---
+++ frontend/walakula/src/App.tsx
@@ -0,0 +1,16 @@
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Register from "./pages/Register";
+
+function App() {
+    return (
+        <div>
+            <Router>
+                <Routes>
+                    <Route path="/register" element={<Register />} />
+                </Routes>
+            </Router>
+        </div>
+    );
+}
+
+export default App;
